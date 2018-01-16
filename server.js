e55da81109a7f00ab2fa07e7ff0fe5cdfa70c1eb
swatantra7 <swatantrajha7@gmail.com>
2018-01-17 00:46:32 +0530

    deployment fixes

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
-const express = require('express')
+const express = require('express');
 const app = express();
-app.use(express.static(_dirname+'/dist'));
+const path = require('path');
+
+app.use(express.static(_dirname + '/dist'));
 app.listen(process.env.PORT || 8080);
 app.get('/*', function(req, res){
-  res.sendFile(path.join(_dirname+'/dist/index.html'));
+  res.sendFile(path.join(_dirname + '/dist/index.html'));
 })
 
 console.log('Console listening!')
