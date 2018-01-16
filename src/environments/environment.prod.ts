edea2a4a97cba02867e70eb8806121b17fe12b41
swatantra7 <swatantrajha7@gmail.com>
2018-01-16 23:50:12 +0530

    initial commit

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,4 @@
 export const environment = {
+  url = 'https://sms-ws.herokuapp.com/'
   production: true
 };
---
+++ src/server.js
@@ -0,0 +1,9 @@
+const express = require('express')
+const app = express();
+app.use(express.static(_dirname+'/dist'));
+app.listen(process.env.PORT || 8080);
+app.get('/*', function(req, res){
+  res.sendFile(path.join(_dirname+'/dist/index.html'));
+})
+
+console.log('Console listening!')
