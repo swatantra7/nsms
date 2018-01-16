45999d5c7d3988fd1b7efd024d54c2ec76e1ca70
swatantra7 <swatantrajha7@gmail.com>
2018-01-17 01:59:40 +0530

    deployment

--- server.js
+++ server.js
@@ -1,14 +1,11 @@
 const express = require('express');
-const http = require('http');
 const app = express();
 const path = require('path');
 
-app.use(express.static(path.join(__dirname + 'dist')));
-var port = process.env.PORT || 5000;
-app.set('port', port);
-const server = http.createServer(app);
-app.get('/*', function(req, res) => {
-  res.sendFile(path.join(__dirname + 'dist/index.html'));
-});
-server.listen(port, () => console.log('Console listening!'));
+app.use(express.static(__dirname + '/dist'));
+app.listen(process.env.PORT || 5000);
+app.get('/*', function(req, res){
+  res.sendFile(path.join(__dirname + '/dist/index.html'));
+})
 
+console.log('Console listening!')
